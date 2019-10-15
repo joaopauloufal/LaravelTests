7248c7d95a759bf966cf659ee2d295731cdf4e6f
joao.silva <joao.silva@arapiraca.al.gov.br>
2019-10-14 21:08:03 -0300

    Fixed

--- app/Account.php
+++ app/Account.php
@@ -9,4 +9,10 @@ class Account extends Model
     protected $table = 'account';
     protected $fillable = ['title', 'agency', 'account_number', 'balance', 'balance_initial', 'bank_id', 'bank_image'];
 
+    public function bank(){
+
+        return $this->belongsTo('App\Bank');
+
+    }
+
 }
