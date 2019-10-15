c576e5b64feaadc6a99d867390146c99cc6ae282
joao.silva <joao.silva@arapiraca.al.gov.br>
2019-10-15 20:25:16 -0300

    Testando remoção e atualização de dados

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -36,11 +36,41 @@ class AccountTest extends TestCase
 
     }
 
+    public function testApiViewNotFound(){
+
+        $response = $this->json('GET', '/api/accounts/1');
+
+        $response->assertStatus(404);
+
+    }
+
     public function testApiInsert(){
 
         $data = factory(\App\Account::class)->make();
 
         $response = $this->json('POST', '/api/accounts/', $data->toArray());
+
+        $response->assertStatus(200)->assertJson($data->toArray());
+
+    }
+
+    public function testApiUpdate(){
+
+        $data = factory(\App\Account::class)->create();
+
+        $toUpdate = ['title'=>'Conta do João'];
+
+        $response = $this->json('PUT', '/api/accounts/'. $data->id, $toUpdate);
+
+        $response->assertStatus(200)->assertJson($toUpdate);
+
+    }
+
+    public function testApiDelete(){
+
+        $data = factory(\App\Account::class)->create();
+
+        $response = $this->json('DELETE', '/api/accounts/'. $data->id);
 
         $response->assertStatus(200)->assertJson($data->toArray());
 
